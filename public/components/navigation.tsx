68b9b47fb446a8d0e028004bc5a061bf2c822b51
William <WilliamNyunt@outlook.com>
2024-05-06 22:12:58 +0100

    First commit

---
+++ public/components/navigation.tsx
@@ -0,0 +1,19 @@
+export default function Navigation() {
+    return (
+        <aside>
+            <nav className=" ">
+                <ul>
+                    <li>
+                        <a href="/">Habits</a>
+                    </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
