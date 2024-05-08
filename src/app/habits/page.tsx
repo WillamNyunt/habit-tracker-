cfc76467037ceb550f4df1c92106a477c73ad7a6
William <WilliamNyunt@outlook.com>
2024-05-08 17:26:17 +0100

    id added to edit button

--- src/app/habits/page.tsx
+++ src/app/habits/page.tsx
@@ -5,13 +5,16 @@ import { getHabits } from "@/lib/habits";
 
 async function Habits() {
   const habits : any =  await getHabits();
- 
+  console.log(habits)
+
   return (
     <>
      {habits.map ((habit : any) => (
         <div key={habit.id} className="flex justify-between">
           <span>{habit.name}</span>
+          <Link href={`?modal=true&type=edit-habit&id=${habit.id}`}>
           <button className="button-primary">Edit</button>
+          </Link>
         </div>
       ))}
     </>
@@ -20,9 +23,6 @@ async function Habits() {
 
 
 export default function HabitsPage() {
-  
-
-
   return (
     <>
       <h1>All habits</h1>
@@ -30,7 +30,7 @@ export default function HabitsPage() {
         <Card className="col-span-2 flex flex-col">
           <div className="flex w-full justify-between">
             <h2>Habits</h2>
-            <Link href="?modal=true&type=habit">
+            <Link href="?modal=true&type=add-habit">
             <button className="button-primary">Add</button>
             </Link>
           </div>
