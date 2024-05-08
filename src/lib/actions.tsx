700d109303f33c30ef56abf2b984b98d63dd73a8
William <WilliamNyunt@outlook.com>
2024-05-08 17:26:27 +0100

    editHabitAction

--- src/lib/actions.tsx
+++ src/lib/actions.tsx
@@ -1,6 +1,6 @@
 'use server'
 
-import { saveHabitDb } from "./habits"
+import { saveHabitDb, editHabitDb, getHabitById } from "./habits"
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 
@@ -17,4 +17,29 @@ export async function addHabitAction(prevState : {message : string}, formData : 
 
     revalidatePath('/habits')
     redirect('/habits')
+}
+
+export async function editHabitAction(prevState : {message : string}, formData : FormData) {
+    const data = {
+        name: formData.get('name') as string,
+        id: formData.get('id') as string
+    }
+
+    console.log(data)
+
+    if (!data.name) {
+        return { message: "Name is required" }
+    }
+
+    await editHabitDb(data)
+
+    revalidatePath('/habits')
+    redirect('/habits')
+}
+
+export async function getHabitByIdAction(id: string) {
+    if (!id) {
+        return { message: "Id is required" }
+    }
+    return await getHabitById(id)
 }
