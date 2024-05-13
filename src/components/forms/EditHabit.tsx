71fefdb0d36ffa5a781826b97b38249fe18cfdfe
William <WilliamNyunt@outlook.com>
2024-05-13 13:36:15 +0100

    Update: Time of day input added

--- src/components/forms/EditHabit.tsx
+++ src/components/forms/EditHabit.tsx
@@ -9,26 +9,33 @@ interface EditHabitFormProps {
 }
 
 interface Habits {
-    name: string;
-    id: string;
-    slug: string;
+  name: string;
+  id: string;
+  slug: string;
 }
 
-const  EditHabitForm : React.FC<EditHabitFormProps> =  (props) => {
-  const [formState, formAction] = useFormState(editHabitAction, { message: "" });
+const EditHabitForm: React.FC<EditHabitFormProps> = (props) => {
+  const [formState, formAction] = useFormState(editHabitAction, {
+    message: "",
+  });
   const searchParams = useSearchParams();
   const id = searchParams.get("id") as string;
-  
+
   const nameInputRef = useRef<HTMLInputElement>(null);
+  const timeOfDayInputRef = useRef<HTMLSelectElement>(null);
 
   useEffect(() => {
     async function fetchHabit() {
-      const habit = await getHabitByIdAction(id) as Habits;
+      const habit = (await getHabitByIdAction(id)) as Habits;
+      console.log
       if (!habit) {
         return;
-      }
+      } 
       if (nameInputRef.current) {
         nameInputRef.current.value = habit.name;
+      }
+      if (timeOfDayInputRef.current) {
+        timeOfDayInputRef.current.value = habit.time_of_day;
       }
     }
     fetchHabit();
@@ -38,15 +45,21 @@ const  EditHabitForm : React.FC<EditHabitFormProps> =  (props) => {
     <form className="flex flex-col gap-3 w-full" action={formAction}>
       <h2>Edit habit</h2>
       <label htmlFor="name">Name</label>
-      <input type="text" name="name" placeholder="Name" />
+      <input
+        ref={nameInputRef}
+        type="text"
+        name="name"
+        placeholder="Name"
+        required
+      />
       <label htmlFor="time_of_day">Time of day</label>
-      <select name="time_of_day">
+      <select ref={timeOfDayInputRef} name="time_of_day">
         <option value="all">All</option>
         <option value="morning">Morning</option>
         <option value="afternoon">Afternoon</option>
         <option value="evening">Evening</option>
       </select>
-        <input type="hidden" name="id" value={id} />
+      <input type="hidden" name="id" value={id} />
       {formState?.message && <p>{formState.message}</p>}
       <div className="flex justify-between mt-3">
         <button
@@ -60,6 +73,6 @@ const  EditHabitForm : React.FC<EditHabitFormProps> =  (props) => {
       </div>
     </form>
   );
-}
+};
 
 export default EditHabitForm;
