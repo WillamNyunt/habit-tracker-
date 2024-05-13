c813c3ee87acfde87352299343c6bad8aae70311
William <WilliamNyunt@outlook.com>
2024-05-13 13:34:40 +0100

    Config file

---
+++ src/lib/config.js
@@ -0,0 +1,8 @@
+export const habitColors = {
+    all: "#000000",        // Black for general or default use
+    morning: "#5980ff",    // Light Blue for morning
+    afternoon: "#e8811a",  // Soft Orange for afternoon
+    evening: "#643bc4"     // Royal Purple for evening
+};
+
+export const habitTitleColor = '#FFFFFF'; // White for text color of habit title
