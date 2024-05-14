2ce1e2b151fba112e81534e09969e4a375d7baf3
William <WilliamNyunt@outlook.com>
2024-05-14 15:03:05 +0100

    [WIP] Habit tracker calendar

--- src/components/habitTrackerCalendar.tsx
+++ src/components/habitTrackerCalendar.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+"use client";
+
+import React from "react";
 import CalenadarHeaTMap from "@/components/CalendarHeatMap";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { useState } from "react";
+import moment from "moment";
 
 const HabitTrackerCalendar = () => {
-    return (
-        <div>
-            <h1>Habit Tracker Calendar</h1>
-            <CalenadarHeaTMap month={5} year={2024} />
-        </div>
-    )
-}
+const [date, setDate] = useState<string | null>(moment().format("YYYY-MM-DD"));
+
+const month = moment(date).year();
+const year = moment(date).year();
+
+return (
+    <div>
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+            <DatePicker
+                label={"Select month"}
+                views={["month", "year"]}
+                value={moment(date)} 
+                onAccept={value => setDate(value!.format("YYYY-MM-DD"))}
+            />    
+        </LocalizationProvider>
+        <CalenadarHeaTMap month={month} year={year} />
+    </div>
+);
+};
 
 export default HabitTrackerCalendar;
-
