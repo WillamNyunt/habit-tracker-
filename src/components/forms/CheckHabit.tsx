44aa67037fb0c58c9f0199263c72a83b2bb9221f
William Nyunt <82210457+WillamNyunt@users.noreply.github.com>
2024-06-04 17:40:45 +0100

    Habit check optmistic update

--- src/components/forms/CheckHabit.tsx
+++ src/components/forms/CheckHabit.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, {useRef} from "react";
 import { checkHabitFormAction } from "@/lib/actions";
 import { useFormState } from "react-dom";
 import classes from "./CheckHabit.module.css";
@@ -14,13 +14,21 @@ import { usePathname } from "next/navigation";
 const CheckHabitForm: React.FC<{ habitId: string, date: string, check: boolean }> = ({ habitId, date, check }) => {
   const [state, formAction] = useFormState(checkHabitFormAction, { message: "" });
   const path = usePathname();
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //* This makes optmistic update to the button to show that the habit has been checked, if invalid the button will revert back to its original state *//
+    buttonRef.current?.classList.add(classes['check-filled']);
+  };
+
 
   return (
-    <form action={formAction}>
+    <form action={formAction} onSubmit={handleFormSubmit}>
       <input type="hidden" name="habit_id" value={habitId} />
       <input type="hidden" name="date" value={date} />
       <input type="hidden" name="revalidatePath" value={path} />
-      <button type="submit" className={check ? classes['check-filled'] : classes['check-empty']}></button>
+      <button type="submit" ref={buttonRef} className={check ? classes['check-filled'] : classes['check-empty']}></button>
       {state?.message && <p>{state.message}</p>}
     </form>
   )
