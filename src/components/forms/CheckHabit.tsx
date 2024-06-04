969d820cbdbdc5a7daf138c61df8b93c7189b310
William Nyunt <82210457+WillamNyunt@users.noreply.github.com>
2024-06-04 20:54:38 +0100

    Pending useFormStatus added

--- src/components/forms/CheckHabit.tsx
+++ src/components/forms/CheckHabit.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, {useRef} from "react";
 import { checkHabitFormAction } from "@/lib/actions";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 import classes from "./CheckHabit.module.css";
 import { usePathname } from "next/navigation";
 
@@ -13,23 +13,17 @@ import { usePathname } from "next/navigation";
  */
 const CheckHabitForm: React.FC<{ habitId: string, date: string, check: boolean }> = ({ habitId, date, check }) => {
   const [state, formAction] = useFormState(checkHabitFormAction, { message: "" });
+  const { pending } = useFormStatus();
   const path = usePathname();
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  
-  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    //* This makes optmistic update to the button to show that the habit has been checked, if invalid the button will revert back to its original state *//
-    buttonRef.current?.classList.add(classes['check-filled']);
-  };
-
 
   return (
-    <form action={formAction} onSubmit={handleFormSubmit}>
+    <form action={formAction}>
       <input type="hidden" name="habit_id" value={habitId} />
       <input type="hidden" name="date" value={date} />
       <input type="hidden" name="revalidatePath" value={path} />
-      <button type="submit" ref={buttonRef} className={check ? classes['check-filled'] : classes['check-empty']}></button>
+      <button type="submit" className={check ? classes['check-filled'] : classes['check-empty']} disabled={pending}></button>
       {state?.message && <p>{state.message}</p>}
+      {pending && <p>Checking...</p>}
     </form>
   )
 }
